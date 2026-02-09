47f5c350409329fbd1d99257ac86490f2f5934a7
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-09 17:15:15 +0200

    add type of a successful API response data / obj

---
+++ trivia/src/requester/types.ts
@@ -0,0 +1,8 @@
+export type questionData = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
