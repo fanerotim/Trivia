633beee49c476edb28d99a087b10f094b0a39dfe
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-09 20:48:33 +0200

    modify requester, so it can be more abstract and reusable

--- trivia/src/requester/requester.ts
+++ trivia/src/requester/requester.ts
@@ -1,12 +1,11 @@
-import {type questionData } from "./types";
+import { type QuestionData } from "./types";
+import { type TokenResponse } from "./types";
 
 export const requester = async (
-    amount: number,
-    category: number,
-    difficulty: string
-): Promise<questionData[] | Error> => {
+    url: string
+): Promise<QuestionData[] | TokenResponse | Error> => {
 
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}amount=${amount}&category=${category}&difficulty=${difficulty}`);
+    const response = await fetch(url);
 
     if (!response.ok) {
         throw Error('Network request failed - please try again.')
