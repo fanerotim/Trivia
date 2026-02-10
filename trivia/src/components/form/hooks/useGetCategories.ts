d387c9a7a913d500b1f73638711e9c0c8af46aac
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-10 21:51:51 +0200

    add initial version of a custom hook that retrieves available trivia categories

---
+++ trivia/src/components/form/hooks/useGetCategories.ts
@@ -0,0 +1,23 @@
+import { requester } from "../../../requester/requester"
+import type { Category, CategoryList } from "../../../requester/types";
+
+export const useGetCategories = () => {
+
+    const getCategories = async (): Promise<Category[] | Error> => {
+
+        try {
+            const response = await requester(import.meta.env.VITE_CATEGORY_LIST_URL) as CategoryList;
+            return response.trivia_categories;
+        } catch (err) {
+            console.log(err);
+            const error = new Error();
+            error.message = 'Network request failed';
+            return error;
+        }
+    }
+
+    return {
+        getCategories
+    }
+}
+
