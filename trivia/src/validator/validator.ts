607bde5adb49510155dd425f8ee974f08cfae3ec
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-12 21:20:10 +0200

    update validator - make sure there are no empty input fields submitted

--- trivia/src/validator/validator.ts
+++ trivia/src/validator/validator.ts
@@ -4,12 +4,12 @@ import { type FormValues } from '../components/form/types/types';
 export const validator = (userInput: FormValues) => {
     const schema = z.object({
         questions_count: z.number().min(10).max(50),
-        category: z.string(),
-        difficulty: z.string()
+        category: z.string().trim().min(3),
+        difficulty: z.string().trim().min(4)
     })
 
     const data = schema.safeParse(userInput);
-    
+
     if (!data.success) {
         const flattenedError = z.flattenError(data.error)
         throw flattenedError;
