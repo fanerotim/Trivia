7b67a950d7aca34b6193c4b3a8a79cf30f3e80a9
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-13 23:26:52 +0200

    remove unnecessary log and Number.isNan() check
    z.number() makes sure we do not accept NaN

--- trivia/src/validator/validator.ts
+++ trivia/src/validator/validator.ts
@@ -2,20 +2,19 @@ import { z } from 'zod';
 import { type FormValues } from '../components/form/types/types';
 
 export const validator = (userInput: FormValues) => {
+
     const schema = z.object({
-        questions_count: z.number().min(10).max(50).refine((val) => !Number.isNaN(val)),
+        questions_count: z.number().min(10).max(50),
         category: z.string().trim().min(3).refine((val) => val !== 'default'),
         difficulty: z.string().trim().min(4).refine((val) => val !== 'default')
     })
 
-    
     const data = schema.safeParse(userInput);
-    
+
     if (!data.success) {
         const flattenedError = z.flattenError(data.error);
-        console.log(flattenedError);
         throw flattenedError;
     }
-    
+
     return data;
 }
