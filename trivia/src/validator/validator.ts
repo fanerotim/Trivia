0a790def57309c685a02ee3ae14940158a6ef169
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-13 16:17:15 +0200

    update validator to check for default values

--- trivia/src/validator/validator.ts
+++ trivia/src/validator/validator.ts
@@ -4,14 +4,16 @@ import { type FormValues } from '../components/form/types/types';
 export const validator = (userInput: FormValues) => {
     const schema = z.object({
         questions_count: z.number().min(10).max(50),
-        category: z.string().trim().min(3),
-        difficulty: z.string().trim().min(4)
+        category: z.string().trim().min(3).refine((val) => val !== 'default'),
+        difficulty: z.string().trim().min(4).refine((val) => val !== 'default')
     })
 
+    
     const data = schema.safeParse(userInput);
-
+    
     if (!data.success) {
         const flattenedError = z.flattenError(data.error);
+        console.log(flattenedError);
         throw flattenedError;
     }
     
