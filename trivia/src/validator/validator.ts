1e4c460ce16fd6809ddc745336f2f270abcbf2f0
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-13 16:21:17 +0200

    update validator again, make sure NaN is not accepted

--- trivia/src/validator/validator.ts
+++ trivia/src/validator/validator.ts
@@ -3,7 +3,7 @@ import { type FormValues } from '../components/form/types/types';
 
 export const validator = (userInput: FormValues) => {
     const schema = z.object({
-        questions_count: z.number().min(10).max(50),
+        questions_count: z.number().min(10).max(50).refine((val) => !Number.isNaN(val)),
         category: z.string().trim().min(3).refine((val) => val !== 'default'),
         difficulty: z.string().trim().min(4).refine((val) => val !== 'default')
     })
