b319af3515e0bc2877961228d387baa93d661389
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-13 15:52:24 +0200

    update form component
    add selected by default options

--- trivia/src/components/form/Form.tsx
+++ trivia/src/components/form/Form.tsx
@@ -1,12 +1,106 @@
 import './Form.scss';
+import { useEffect, useState } from 'react';
+import { useGetCategories } from './hooks/useGetCategories';
+import { type Category } from '../../requester/types';
+import type { FormValues } from './types/types';
+import { useFormHandler } from './hooks/useFormHandler';
+
+const initialValues: FormValues = {
+    questions_count: 0,
+    category: '',
+    difficulty: ''
+}
 
 export const Form = () => {
-    
-    return (
-        <form>
-            <select>
 
-            </select>
+    const [categories, setCategories] = useState<Category[]>([]);
+    const { getCategories } = useGetCategories();
+    const { handleChange, handleSubmit } = useFormHandler(initialValues);
+
+    useEffect(() => {
+        const categories = async () => {
+            const categoryList = await getCategories() as Category[];
+            setCategories(categoryList)
+        }
+        categories();
+    }, [])
+
+    // set length of arr to 50, so we can then fill it up
+    let arr: number[] = [];
+    arr.length = 50;
+    arr.fill(1, 0, 50);
+
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+        >
+
+            <div>
+                <label htmlFor='questions_count'>Choose number of questions</label>
+                <select
+                    onChange={handleChange}
+                    name='questions_count'
+                    id='questions_count'
+                >
+                    <option
+                        value="default"
+                        selected
+                    >
+                        Select an option
+                    </option>
+                    {arr.map((_, i) => {
+                        return (
+                            <option key={i}>{i + 1}</option>
+                        )
+                    })}
+                </select>
+            </div>
+
+            <div>
+                <label
+                    htmlFor='category'
+                >
+                    Choose category
+                </label>
+                <select
+                    onChange={handleChange}
+                    name='category'
+                    id='category'
+                >
+                    <option
+                        value="default"
+                        selected
+                    >
+                        Select an option
+                    </option>
+                    {categories.map(c => {
+                        return (
+                            <option key={c.id}>{c.name}</option>
+                        )
+                    })}
+                </select>
+            </div>
+
+            <div>
+                <label
+                    htmlFor='difficulty'
+                >
+                        Choose difficulty
+                </label>
+                <select
+                    onChange={handleChange}
+                    name='difficulty'
+                    id='difficulty'
+                >
+                    <option value="default" selected>Select an option</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="difficult">Difficult</option>
+                </select>
+            </div>
+            <input type="submit" />
+
         </form>
     )
 }
