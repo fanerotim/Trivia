c5e21d38114289d395d988b60a3e1f21542acef8
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-16 21:52:00 +0200

    configure react-router-dom

--- trivia/src/components/home/Home.tsx
+++ trivia/src/components/home/Home.tsx
@@ -1,8 +1,12 @@
 import './Home.scss';
+import { Form } from '../form/Form';
 
 export const Home = () => {
-    
+
     return (
-        <h1>Welcome to Trivia</h1>
+        <>
+            <h1>Welcome to Trivia</h1>
+            <Form />
+        </>
     )
 }
