f06a8a26dc6b5f4173f22f458f1d5926d6bd11e2
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-16 17:42:00 +0200

    add logic to reset error state on submit to remove any old errors

--- trivia/src/components/form/hooks/useFormHandler.ts
+++ trivia/src/components/form/hooks/useFormHandler.ts
@@ -31,10 +31,23 @@ export const useFormHandler = (initialValues: FormValues) => {
     const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
 
+        // reset error state on submit to remove any old error messages
+        setError((_) => {
+            return {
+                fieldErrors: {
+                    category: [],
+                    difficulty: [],
+                    questions_count: [],
+                },
+                formErrors: []
+            }
+        })
+
         try {
             validator(values);
             // handle success case: load / redirect to a page, where user can start playing the game
         } catch (err) {
+            // update error state to the returned error
             setError((_) => err as ErrorData)
         }
     }
@@ -42,6 +55,6 @@ export const useFormHandler = (initialValues: FormValues) => {
     return {
         handleChange,
         handleSubmit,
-        error 
+        error
     }
 }
