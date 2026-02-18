eef34f9fcb00d74fa6231f2c54b9f3dbc040cd55
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-18 14:39:15 +0200

    add custom hook that is responsible for token handling

---
+++ trivia/src/auth/useHandleToken.ts
@@ -0,0 +1,36 @@
+import { requester } from "../requester/requester";
+import type { TokenResponse } from "../requester/types";
+
+export const useHandleToken = () => {
+
+    const setToken = async () => {
+
+        // check if we already have token - if we do, do not set another one
+        const hasToken = getToken();
+
+        if (hasToken) {
+            return;
+        }
+
+        // if we do not have a token, then set one
+        const response = await requester(import.meta.env.VITE_TOKEN_REQ_URL) as TokenResponse;
+        const token = response.token;
+        sessionStorage.setItem('token', token);
+    }
+
+    const getToken = () => {
+        
+        const token = sessionStorage.getItem('token');
+
+        if (token) {
+            return token;
+        }
+
+        return 'No token found'
+    }
+
+    return {
+        setToken,
+        getToken
+    }
+}
