8da10b492e1b8193445eff72754ffc39ccf5f347
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-18 14:38:59 +0200

    remove unnecessary logic

--- trivia/src/auth/requestToken.ts
+++
@@ -1,6 +0,0 @@
-import { requester } from "../requester/requester";
-
-export const requestToken = async () => {
-    const response = await requester(import.meta.env.VITE_TOKEN_REQ_URL);
-    return response;
-}
