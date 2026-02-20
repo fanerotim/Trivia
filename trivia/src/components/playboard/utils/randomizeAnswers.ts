7e88343fc8574d14e92c70ec3ba2bc97629bfa6b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-20 15:18:51 +0200

    update util responsible for randomizing answers array - make it clearer to read / understand and make sure switch cases are randomly chosen

--- trivia/src/components/playboard/utils/randomizeAnswers.ts
+++ trivia/src/components/playboard/utils/randomizeAnswers.ts
@@ -1,29 +1,33 @@
 import { type QuestionData } from "../../../requester/types";
 
+// possible cases hardcoded as I want to return a some sort of random array, not as random as possible algorithmic solution
+const cases: string[] = ['reverse', 'pop', 'shift', 'randomIndex'];
+
 export const randomizeAnswers = (questionData: QuestionData) => {
 
     let answers: string[] = [questionData.correct_answer, ...questionData.incorrect_answers];
     const answerCount = answers.length;
 
     for (let i = 0; i < answerCount; i++) {
+        const currentCase = generateCurrentCase(answerCount);
         const randomNum = generateRandomNumber(answerCount);
 
-        switch (randomNum) {
-            case 0:
+        switch (currentCase) {
+            case 'reverse':
                 // simply reverse order of array elements
-                answers = answers.reverse();
+                answers.reverse();
                 break;
-            case 1:
+            case 'pop':
                 // remove last element of answers array and place it at index 1
                 const poppedElement: string = answers.pop()!;
                 answers.splice(randomNum, 0, poppedElement)
                 break;
-            case 2:
+            case 'shift':
                 // remove first element of answers array and place it at the end of the array
                 const unshiftedElement: string = answers.shift()!;
                 answers.push(unshiftedElement);
                 break;
-            case 3:
+            case 'randomIndex':
                 // remove last element and place it at a randomly generated index
                 const element = answers.pop()!;
                 const index = generateRandomNumber(answerCount);
@@ -37,4 +41,9 @@ export const randomizeAnswers = (questionData: QuestionData) => {
 const generateRandomNumber = (count: number) => {
     const num = Math.floor(Math.random() * count);
     return num;
+}
+
+const generateCurrentCase = (answerCount: number) => {
+    const caseIndex = generateRandomNumber(answerCount);
+    return cases[caseIndex]
 }
