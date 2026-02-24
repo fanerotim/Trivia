02301e0474292d94dc0b33b3847c74a0831a285d
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-24 22:54:21 +0200

    add basic logic that highlights selected answer and adds specific class depending on whether the user has answered correctly or not
    also render jsx conditionally, based on whether the user has answered correctly or not

--- trivia/src/components/playboard/Playboard.tsx
+++ trivia/src/components/playboard/Playboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import './Playboard.scss';
@@ -11,11 +11,13 @@ export const Playboard = () => {
 
     // index controls the current question; when index changes, the questions switches to the next one
     const [index, setIndex] = useState<number>(0);
+    const [answers, setAnswers] = useState<string[]>([])
 
     // state that represents user submitted answer: it is null when user has not answers; else it is true / false
     const [answerState, setAnswerState] = useState<AnswerState>({
         userAnswer: '',
-        hasAnswered: false
+        hasAnswered: false,
+        isCorrect: null
     });
 
     // location is used to get the questions from its 'state' object
@@ -25,20 +27,25 @@ export const Playboard = () => {
     // store correct answer in a constant
     const correctAnswer = questions[index].correct_answer;
 
-    // generate an array of answers in random order
-    const answers = randomizeAnswers(questions[index]);
+    // update answers state; this is not optimized, but it solves an in issue whereby order of answers change if we select an answer twice
+    if (answers.length == 0) {
+        const currentAnswers = randomizeAnswers(questions[index]);
+        setAnswers((_) => currentAnswers)
+    }
 
     // This is a possible approach, but far from final
     // TODO: complete this functionality
     const handleClick = (userAnswer: string) => {
 
-        // block further clicks if user has answered already
-        if (answerState.hasAnswered) {
+        // if user has already submitted their answer return; do not allow further clicks
+        if (answerState.isCorrect !== null) {
             return;
         }
+
         // update state with correct_answer
-        setAnswerState((_) => {
+        setAnswerState((prev) => {
             return {
+                ...prev,
                 hasAnswered: true,
                 userAnswer: userAnswer,
             }
@@ -46,6 +53,15 @@ export const Playboard = () => {
 
         // update index, so we can move to next question: this should be controlled by another handler for next btn; not yet added
         // setIndex((prev) => prev + 1);
+    }
+
+    const checkAnswer = () => {
+        setAnswerState((prev) => {
+            return {
+                ...prev,
+                isCorrect: correctAnswer === answerState.userAnswer
+            }
+        })
     }
 
     return (
@@ -62,15 +78,21 @@ export const Playboard = () => {
                             className={
                                 `${a === answerState.userAnswer
                                     ? 'selected_answer'
-                                    : ''} 
-                                ${answerState.hasAnswered && a === correctAnswer 
-                                    ? 'correct_answer' 
-                                    : ''}`}
+                                    : ''}
+                                    
+                                ${answerState.userAnswer === a && answerState.isCorrect === true
+                                    ? 'correct_answer'
+                                    : answerState.userAnswer === a && answerState.isCorrect === false
+                                        ? 'incorrect_answer' : ''}    
+                                    `}
                         >
                             {decodeHTMLEntity(a)}
                         </p>
                     )
                 })}
+                {answerState.hasAnswered && <button onClick={checkAnswer}>Check answer</button>}
+                {answerState.userAnswer !== '' && answerState.isCorrect ? 'Correct!' : ''}
+                {answerState.hasAnswered === true && answerState.isCorrect === false ? `Correct answer is ${correctAnswer}` : ''}
             </div>
         </>
     )
