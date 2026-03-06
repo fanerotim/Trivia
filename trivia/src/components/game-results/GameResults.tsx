9b556a781fb9201d625f12e32a9895e8b97a5d6b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-06 23:27:20 +0200

    add a button to start a new game (if user wants)

--- trivia/src/components/game-results/GameResults.tsx
+++ trivia/src/components/game-results/GameResults.tsx
@@ -1,15 +1,22 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const GameResults = () => {
 
     const location = useLocation();
     const score = location.state;
+    
+    const navigate = useNavigate();
+
+    const handlePlayAgain = () => {
+        navigate('/');
+    }
 
     return (
         <>
             <h1>Game Over!</h1>
             <h4>You answered {score} questions correctly!</h4>
-            <p>Want to play again?</p>
+            <button onClick={handlePlayAgain}>Play again</button>
         </>
     )
 }
