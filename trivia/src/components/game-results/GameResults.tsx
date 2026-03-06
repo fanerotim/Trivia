934fb1a0d6ebbb86a31dec9f5265590b486b7d14
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-06 23:25:13 +0200

    show score / correct answers to user

--- trivia/src/components/game-results/GameResults.tsx
+++ trivia/src/components/game-results/GameResults.tsx
@@ -3,11 +3,12 @@ import { useLocation } from "react-router-dom"
 export const GameResults = () => {
 
     const location = useLocation();
-    console.log(location.state);
+    const score = location.state;
 
     return (
         <>
             <h1>Game Over!</h1>
+            <h4>You answered {score} questions correctly!</h4>
             <p>Want to play again?</p>
         </>
     )
