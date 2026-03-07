0caa80ce7bb0ed676cf59f72946a0205e31b1569
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-07 22:59:21 +0200

    add initial implementation of a custom drop-down menu / select

--- trivia/src/components/form/components/Select.tsx
+++ trivia/src/components/form/components/Select.tsx
@@ -1,10 +1,35 @@
 import './Select.scss';
+import { type Category } from '../../../requester/types';
+import { useSelect } from './hooks/useSelect';
 
-export const Select = () => {
+export const Select = (
+    { categories }: { categories: Category[] }
+) => {
+
+    const { isSelectOpen, handleToggle } = useSelect();
+
     return (
-        < div >
-            <h1>Custom select</h1>
+        <article>
+            < div
+                onClick={handleToggle}
+                className='select__container'
+            >
+                <p>Select a category</p>
+                {isSelectOpen
+                    ? <span className='select__container__close__btn'></span>
+                    : <span className='select__container__open__btn'></span>
+                }
 
-        </div >
+
+            </div >
+
+            <div
+                className={`select__options__container ${isSelectOpen ? 'select__options__container__open' : ''}`}
+            >
+                {categories.map((c) => (
+                    <span key={c.id}>{c.name}</span>
+                ))}
+            </div>
+        </article>
     )
 }
