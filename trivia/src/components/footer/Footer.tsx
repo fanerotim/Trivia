0e609f630a76aa7b031003e896074647444cdff2
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-10 17:42:03 +0200

    add footer component

---
+++ trivia/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import './Footer.scss';
+
+export const Footer = () => {
+    return (
+        <div
+            className='footer__container'
+        >
+            <p
+                className='footer__container__text'
+            >
+                Petar Cholakov, 2026. All rights reserved.
+            </p>
+        </div>
+
+    )
+}
