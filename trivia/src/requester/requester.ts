4aac2f6213df1b5454d186332b75cede4d7fd6b3
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-10 16:52:51 +0200

    add logic to reset token if an issue with the current token was detected

--- trivia/src/requester/requester.ts
+++ trivia/src/requester/requester.ts
@@ -4,16 +4,28 @@ import type {
     CategoryList
 } from "./types";
 
+import { useHandleToken } from "../auth/useHandleToken";
+
 export const requester = async (
     url: string
 ): Promise<QuestionData[] | TokenResponse | CategoryList | Error> => {
 
-    const response = await fetch(url);
+    const {resetToken} = useHandleToken();
 
+    const response = await fetch(url);
+    
     if (!response.ok) {
         throw Error('Network request failed - please try again.')
     }
 
     const data = await response.json();
+    
+    // get API response code; invalid token returns response_code 3; according to their documentation
+    const apiResponseCode = data.response_code
+    // if token is invalid e.g. response_code !== 0
+    if (apiResponseCode !== 0) {
+        resetToken();
+    }
+
     return data;
 }
