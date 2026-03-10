ff8df277566e5a375454bce0d73a110c2d949a74
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-10 21:52:24 +0200

    add a handler and state to useSelect hook that will handle custom select / dropdown menu changes

--- trivia/src/components/form/components/select/hooks/useSelect.tsx
+++ trivia/src/components/form/components/select/hooks/useSelect.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
 export const useSelect = () => {
-    
+
     const [isSelectOpen, setIsSelectOpen] = useState(false);
+    const [optionValue, setOptionValue] = useState('Select a category');
 
     const handleToggle = () => {
         setIsSelectOpen((prev) => !prev);
     }
 
+    const handleSelectedOption = (value: string) => {
+        setOptionValue(value);
+        handleToggle();
+    }
+
     return {
         isSelectOpen,
-        handleToggle
+        handleToggle,
+        optionValue,
+        handleSelectedOption
     }
 }
