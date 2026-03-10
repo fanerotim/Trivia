10a8ad3c17f6ab315e113a67d3489f5f2c203ecd
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-10 17:04:00 +0200

    add basic logic to handle error if we have not received results (questions and answers) from API

--- trivia/src/components/playboard/components/Error/Error.tsx
+++ trivia/src/components/playboard/components/Error/Error.tsx
@@ -1,7 +1,23 @@
 import './Error.scss';
+import { useNavigate } from 'react-router-dom';
 
 export const Error = () => {
+
+    const navigate = useNavigate();
+
+    const submitAgainHandler = () => {
+        navigate('/');
+    }
+
     return (
-        <h1>Error!</h1>       
+        <div>
+            <h1>Error!</h1>
+            <button
+                onClick={submitAgainHandler}
+                className='submit__btn'
+            >
+                Submit Form Again
+            </button>
+        </div>
     )
 }
