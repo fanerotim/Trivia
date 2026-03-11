7e8cd31e1b9415b6e3a45c4d4f85f5c3415586dc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-11 17:31:29 +0200

    add a util that generates difficulty options

---
+++ trivia/src/components/form/utils/generateDifficultyValues.ts
@@ -0,0 +1,21 @@
+import { type SelectValues } from "../components/select/types/selectValues";
+
+export const generateDifficultyValues = () => {
+    const difficulties = ['easy', 'medium', 'hard'];
+
+    const difficultyValues: SelectValues = [];
+
+    for (let i = 0; i < difficulties.length; i++) {
+        
+        const current = {
+            id: i + 1,
+            name: difficulties[i]
+        }
+
+        difficultyValues.push(current);
+    }
+
+    return {
+        difficultyValues
+    }
+}
