3695ee1a1f67186ceef2173f5cef8f8e03eecc21
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-11 17:31:57 +0200

    remove html select and option elements and use custom select

--- trivia/src/components/form/Form.tsx
+++ trivia/src/components/form/Form.tsx
@@ -5,12 +5,14 @@ import { Error } from '../error/Error';
 import { generateQuestionsCount } from './utils/generateQuestionsCount.ts';
 import { initialValues } from './utils/initialFormValues.ts';
 import { Select } from './components/select/Select';
+import { generateDifficultyValues } from './utils/generateDifficultyValues.ts';
 
 export const Form = () => {
 
+    const { handleSubmit, error } = useFormHandler(initialValues);
     const { categories } = useGetCategories();
-    const { handleChange, handleSubmit, error } = useFormHandler(initialValues);
     const { numberOfQuestions } = generateQuestionsCount();
+    const { difficultyValues } = generateDifficultyValues();
 
     return (
         <form
@@ -24,29 +26,6 @@ export const Form = () => {
             <div
                 className='form__container__form__field'
             >
-                <label
-                    htmlFor='questions_count'
-                    className='form__container__form__field__label'
-                >
-                    Choose number of questions
-                </label>
-                <select
-                    onChange={handleChange}
-                    name='questions_count'
-                    id='questions_count'
-                    defaultValue={'default'}
-                >
-                    <option
-                        value="default"
-                    >
-                        Select an option
-                    </option>
-                    {numberOfQuestions.map((q) => {
-                        return (
-                            <option key={q.id}>{q.name}</option>
-                        )
-                    })}
-                </select>
 
                 {error.fieldErrors.questions_count ? <Error message={error.fieldErrors.questions_count} /> : ''}
             </div>
@@ -58,29 +37,6 @@ export const Form = () => {
                     values={numberOfQuestions}
                     label={'number of questions'}
                 />
-                <label
-                    htmlFor='category'
-                    className='form__container__form__field__label'
-                >
-                    Choose category
-                </label>
-                <select
-                    onChange={handleChange}
-                    name='category'
-                    id='category'
-                    defaultValue={'default'}
-                >
-                    <option
-                        value="default"
-                    >
-                        Select an option
-                    </option>
-                    {categories.map(c => {
-                        return (
-                            <option key={c.id}>{c.name}</option>
-                        )
-                    })}
-                </select>
 
                 {error.fieldErrors.category ? <Error message={error.fieldErrors.category} /> : ''}
 
@@ -89,23 +45,11 @@ export const Form = () => {
             <div
                 className='form__container__form__field'
             >
-                <label
-                    htmlFor='difficulty'
-                    className='form__container__form__field__label'
-                >
-                    Choose difficulty
-                </label>
-                <select
-                    onChange={handleChange}
-                    name='difficulty'
-                    id='difficulty'
-                    defaultValue={'default'}
-                >
-                    <option value="default">Select an option</option>
-                    <option value="easy">Easy</option>
-                    <option value="medium">Medium</option>
-                    <option value="hard">Hard</option>
-                </select>
+
+                <Select
+                    values={difficultyValues}
+                    label={'difficulty'}
+                />
 
                 {error.fieldErrors.difficulty ? <Error message={error.fieldErrors.difficulty} /> : ''}
             </div>
