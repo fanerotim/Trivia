e3bafef53f2c427ca1cb216889cc60cbec1661ee
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-12 21:41:02 +0200

    add classes to error message component and basic styles

--- trivia/src/components/error/Error.tsx
+++ trivia/src/components/error/Error.tsx
@@ -4,8 +4,14 @@ import type { ErrorProps } from './types';
 export const Error = ({ message }: ErrorProps) => {
 
     return (
-        <div>
-            <p>{message[0]}</p>
+        <div
+            className='error__message__container'
+        >
+            <p
+                className='error__message__container__text'
+            >
+                {message[0]}
+            </p>
         </div>
     )
 }
