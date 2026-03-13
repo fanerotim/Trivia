75d9b5914e4823feec7c181c8e5627c2e2c26d24
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-13 22:43:15 +0200

    add a Header component with basic styles for now

---
+++ trivia/src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.scss';
+
+export const Header = () => {
+    return (
+        <header
+            className='header'
+        >
+            <h1
+                className='header__logo'
+            >
+                Brainy
+            </h1>
+        </header>
+    )
+}
