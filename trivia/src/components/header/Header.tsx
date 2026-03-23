907792b25e73213b6f762c7bb8471c89844abf00
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-23 20:45:21 +0200

    install react-icons and import dark and light mode icons

--- trivia/src/components/header/Header.tsx
+++ trivia/src/components/header/Header.tsx
@@ -1,15 +1,25 @@
 import './Header.scss';
+import { MdLightMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
 export const Header = () => {
     return (
         <header
             className='header'
         >
+
             <h1
                 className='header__logo'
             >
                 Brainy
             </h1>
+
+            <div
+                className='header__dark__light__mode__icons__container'
+            >
+                <MdLightMode />
+                <MdDarkMode />
+            </div>
         </header>
     )
 }
