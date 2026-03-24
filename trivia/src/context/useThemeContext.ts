4fb628adf6a28b9db6a64693d6073cd547f04c96
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-24 18:24:45 +0200

    improve custom hook to throw an error if context is used in an outside component

--- trivia/src/context/useThemeContext.ts
+++ trivia/src/context/useThemeContext.ts
@@ -1,7 +1,11 @@
 import { useContext } from "react";
-import { ThemeContext } from "./themeContext";
+import { ThemeContext } from './ThemeContext';
 
 export const useThemeContext = () => {
     const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw Error('ThemeContext must be used by components that have access to it.')
+    }
     return context;
 }
